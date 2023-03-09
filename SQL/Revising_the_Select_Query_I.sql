68c2e1373ec137be2aa2ba390798fbcf2e89cd4c
Mian Awais <mianawais4832@hotmail.com>
2023-03-09 22:22:59 +0500

    Finished SQL -> Select -> Revising the Select Query - 1

--- SQL/Revising_the_Select_Query_I.sql
+++ SQL/Revising_the_Select_Query_I.sql
@@ -2,6 +2,7 @@
 Query all columns for all American cities in CITY with populations larger than 100,000. The CountryCode for America is USA.
 Input Format
 The CITY table is described as follows:
+
 +-------------+--------------+
 | Field       | Type         |
 +-------------+--------------+

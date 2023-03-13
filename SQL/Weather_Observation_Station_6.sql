62e44539810f0c39ddd49fe845d49b00ab730af4
Mian Awais <mianawais4832@hotmail.com>
2023-03-13 17:02:20 +0500

    Finished SQL -> Basic Select -> Weather Observation Station 6

---
+++ SQL/Weather_Observation_Station_6.sql
@@ -0,0 +1,26 @@
+/*
+Weather Observation Station 6
+=============================
+
+Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+
+Input Format
+
+The STATION table is described as follows:
+
++-------------+------------+
+| Field       |   Type     |
++-------------+------------+
+| ID          | INTEGER    |
+| CITY        | VARCHAR(21)|
+| STATE       | VARCHAR(2) |
+| LAT_N       | NUMERIC    |
+| LONG_W      | NUMERIC    |
++-------------+------------+
+
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+*/
+
+SELECT DISTINCT CITY
+FROM STATION
+WHERE CITY LIKE '[aeiou]%';

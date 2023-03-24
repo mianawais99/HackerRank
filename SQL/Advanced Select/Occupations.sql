4518cb5541182a9fb1b076e44bd2f380fac04e3a
Mian Awais <hakiktan@gmail.com>
2023-03-25 00:26:42 +0500

    Finished SQL -> Advanced Select -> Occupations

---
+++ SQL/Advanced Select/Occupations.sql
@@ -0,0 +1,47 @@
+/*
+Occupations
+===========
+Pivot the Occupation column in OCCUPATIONS so that each Name is sorted alphabetically and displayed underneath its corresponding Occupation. The output column headers should be Doctor, Professor, Singer, and Actor, respectively.
+
+Note: Print NULL when there are no more names corresponding to an occupation.
+
+Input Format
+
+The OCCUPATIONS table is described as follows:
+
++-------------+------------+
+| Column      |   Type     |
++-------------+------------+
+| Name        | String     |
+| Occupation  | String     |
++-------------+------------+
+
+Occupation will only contain one of the following values: Doctor, Professor, Singer or Actor.
+
+*/
+
+/* 
+Here's the MS SQL query for the Problem
+*/
+
+select
+    Doctor,
+    Professor,
+    Singer,
+    Actor
+from (
+    select
+        NameOrder,
+        max(case Occupation when 'Doctor' then Name end) as Doctor,
+        max(case Occupation when 'Professor' then Name end) as Professor,
+        max(case Occupation when 'Singer' then Name end) as Singer,
+        max(case Occupation when 'Actor' then Name end) as Actor
+    from (
+            select
+                Occupation,
+                Name,
+                row_number() over(partition by Occupation order by Name ASC) as NameOrder
+            from Occupations
+         ) as NameLists
+    group by NameOrder
+    ) as Names;
